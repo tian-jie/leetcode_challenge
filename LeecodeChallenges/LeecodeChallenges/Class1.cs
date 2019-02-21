8eb111983ec9e153683d1695780e4caa001fc506
JieTi <JieTi@LAPTOP-JIE-TIAN>
2019-02-21 09:49:51 +0800

    code

--- LeecodeChallenges/LeecodeChallenges/Class1.cs
+++ LeecodeChallenges/LeecodeChallenges/Class1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace LeecodeChallenges
 {
@@ -12,30 +13,50 @@ namespace LeecodeChallenges
             {
                 return result;
             }
-
+            var numsList = new List<int>(nums);
+            numsList.Sort();
+            nums = numsList.ToArray();
 
             // 要先排序啊先排序
             var length = nums.Length;
 
             for (var i = 0; i < length; i++)
             {
+                //Debug.WriteLine(new DateTime().Ticks);
+                if (nums[i] > 0)
+                {
+                    break;
+                }
+                var temp1 = nums[i];
                 for (var j = i + 1; j < length; j++)
                 {
-                    var temp = nums[i] + nums[j];
-                    for (var k = j + 1; k < length; k++)
+                    var temp2 = temp1 + nums[j];
+                    if (temp2 > 0)
                     {
-                        if (nums[i] + nums[j] + nums[k] == 0)
+                        break;
+                    }
+                    // 最后一个数折半查找？？？
+                    var k = j + 1;
+                    var indexL = k;
+                    var indexR = length - 1;
+                    var index = indexL + (indexR - indexL + 1) / 2;
+                    while (indexR >= indexL && index >= indexL && index <= indexR)
+                    {
+                        //Debug.WriteLine("{0}:{3}, {1}:{4}, {2}:{5}", i, j, index, nums[i], nums[j], nums[index]);
+                        if (temp2 + nums[index] > 0)
                         {
-                            var newArray = new List<int>()
-                            {
-                                nums[i], nums[j], nums[k]
-                            };
-                            newArray.Sort();
-
+                            indexR = index-1 ;
+                        }
+                        else if (temp2 + nums[index] < 0)
+                        {
+                            indexL = index+1;
+                        }
+                        else if (temp2 + nums[index] == 0)
+                        {
                             var isEqual = false;
                             foreach (var r in result)
                             {
-                                if(newArray[0] == r[0] && newArray[1] == r[1] && newArray[2] == r[2])
+                                if (nums[i] == r[0] && nums[j] == r[1] && nums[index] == r[2])
                                 {
                                     isEqual = true;
                                     break;
@@ -43,11 +64,35 @@ namespace LeecodeChallenges
                             }
                             if (!isEqual)
                             {
-                                result.Add(newArray);
+                                result.Add(new List<int>()
+                                    {
+                                        nums[i], nums[j], nums[index]
+                                    });
+                            }
+                            break;
+                        }
+
+                        index = (indexR + indexL) / 2;
+                    }
+                    if (index >= indexL && index <= indexR && temp2 + nums[index] == 0)
+                    {
+                        var isEqual = false;
+                        foreach (var r in result)
+                        {
+                            if (nums[i] == r[0] && nums[j] == r[1] && nums[index] == r[2])
+                            {
+                                isEqual = true;
+                                break;
                             }
                         }
+                        if (!isEqual)
+                        {
+                            result.Add(new List<int>()
+                            {
+                                nums[i], nums[j], nums[index]
+                            });
+                        }
                     }
-
                 }
 
             }
