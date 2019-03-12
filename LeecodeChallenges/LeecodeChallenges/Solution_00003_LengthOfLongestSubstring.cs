ee3a9cae603fc159b7f94f7a0bad650527ad3f21
Jie Tian <tian_jie@outlook.com>
2019-03-12 22:49:22 +0800

    3,12

---
+++ LeecodeChallenges/LeecodeChallenges/Solution_00003_LengthOfLongestSubstring.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace LeecodeChallenges
+{
+    public class Solution_00003_LengthOfLongestSubstring
+    {
+        const int BUFFER = 95;
+        public int LengthOfLongestSubstring1(string s)
+        {
+            // 创建一个字母mapping
+            var dic = new Dictionary<char, int>();
+            var maxLength = 0;
+
+
+
+            foreach (var c in s)
+            {
+                var cLen = 0;
+                if (dic.ContainsKey(c))
+                {
+                    cLen = dic[c];
+                    // 如果有，看数字是不是最长，最长就替换maxLength
+                    if (maxLength < cLen)
+                    {
+                        maxLength = cLen;
+                    }
+                    dic[c] = 0;
+                    List<char> list = new List<char>();
+                    list.AddRange(dic.Keys);
+                    foreach (var c1 in list)
+                    {
+                        // 所有value>dic[c]的都要设置为0
+                        if (dic[c1] > cLen)
+                        {
+                            if (maxLength < dic[c1])
+                            {
+                                maxLength = dic[c1];
+                            }
+                            dic.Remove(c1);
+                        }
+                    }
+                }
+
+                {
+                    dic[c] = 0;
+
+                    List<char> list = new List<char>();
+                    list.AddRange(dic.Keys);
+                    foreach (var c1 in list)
+                    {
+                        dic[c1]++;
+                    }
+                }
+            }
+
+            foreach (var v in dic.Values)
+            {
+                if (maxLength < v)
+                {
+                    maxLength = v;
+                }
+            }
+
+            return maxLength;
+        }
+
+
+        public int LengthOfLongestSubstring(string s)
+        {
+            // 创建一个字母mapping
+            var dic = new int[BUFFER];
+            for (var i = 0; i < BUFFER; i++)
+            {
+                dic[i] = -1;
+            }
+            var maxLength = 0;
+
+
+            foreach (var c0 in s)
+            {
+                var c = c0 - ' ';
+                var cLen = 0;
+                if (dic[c] >= 0)
+                {
+                    cLen = dic[c];
+                    // 如果有，看数字是不是最长，最长就替换maxLength
+                    if (maxLength < cLen)
+                    {
+                        maxLength = cLen;
+                    }
+                    
+
+                    //for (var i = 0; i < BUFFER; i++)
+                    //{
+                    //    var c1 = dic[i];
+                    //    // 所有value>dic[c]的都要设置为0
+                    //    if (c1 > cLen)
+                    //    {
+                    //        if (maxLength < c1)
+                    //        {
+                    //            maxLength = c1;
+                    //        }
+                    //        dic[i] = -1;
+                    //    }
+                    //}
+                }
+
+                {
+                    dic[c] = 0;
+
+                    for (var i = 0; i < BUFFER; i++)
+                    {
+                        var c1 = dic[i];
+                        if (cLen > 0 && c1 > cLen)
+                        {
+                            // 所有value>dic[c]的都要删掉
+                            if (maxLength < c1)
+                            {
+                                maxLength = c1;
+                            }
+                            dic[i] = -1;
+                        }
+
+                        if (dic[i] >= 0)
+                        {
+                            dic[i]++;
+                        }
+                    }
+                }
+            }
+
+            for (var i = 0; i < BUFFER; i++)
+            {
+                if (maxLength < dic[i])
+                {
+                    maxLength = dic[i];
+                }
+            }
+
+            return maxLength;
+        }
+    }
+}
---
+++ LeecodeChallenges/LeecodeChallenges/Solution_12_IntToRoman.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace LeecodeChallenges
+{
+    public class Solution_12_IntToRoman
+    {
+        public string IntToRoman(int num)
+        {
+            var n = num;
+            var level = 0;
+            var s = "";
+
+            while (n > 0)
+            {
+                var n1 = n % 10;
+                n = n / 10;
+
+
+                var r = ConvertByLevel(level++, n1);
+                s = r + s;
+            }
+
+            return s;
+        }
+
+        static string LevelMap = "IVXLCDM";
+        private string ConvertByLevel(int level, int num)
+        {
+            if (num == 0)
+            {
+                return "";
+            }
+            var c1 = LevelMap[level * 2];
+            var c2 = '0';
+            var c3 = '0';
+            if (level < 3)
+            {
+                c2 = LevelMap[level * 2 + 1];
+                c3 = LevelMap[level * 2 + 2];
+            }
+            string s = "";
+
+            switch (num)
+            {
+                case 1:
+                    s += c1;
+                    break;
+                case 2:
+                    s += c1;
+                    s += c1;
+                    break;
+                case 3:
+                    s += c1;
+                    s += c1;
+                    s += c1;
+                    break;
+                case 4:
+                    s += c1;
+                    s += c2;
+                    //sb.Append(c1);
+                    //sb.Append(c2);
+
+                    break;
+                case 5:
+                    s += c2;
+                    //sb.Append(c2);
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    s += c2;
+                    //sb.Append(c2);
+                    for (var i = 0; i < num - 5; i++)
+                    {
+                        s += c1;
+                        //sb.Append(c1);
+                    }
+                    break;
+                case 9:
+                    s += c1;
+                    s += c3;
+                    //sb.Append(c1);
+                    //sb.Append(c3);
+                    break;
+            }
+
+            return s;
+
+        }
+    }
+}
