e71f756ad1efb953d36512c2f84914a5b61596bc
JieTi <JieTi@LAPTOP-JIE-TIAN>
2019-03-15 17:21:05 +0800

    42

---
+++ LeecodeChallenges.Test/OnlyTest.test.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace LeecodeChallenges.Test
+{
+    public class OnlyTest_Test
+    {
+        [Fact]
+        public void ThreeSum_1()
+        {
+            OnlyTest only = new OnlyTest();
+            only.Test();
+        }
+    }
+}
