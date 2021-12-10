e56de9848059b986a28c7523b17d31a2bb749e22
Jie Tian <jie.tian@innocellence.com>
2021-12-10 09:48:53 +0800

    olution_01480_RunningSumOf1dArray

---
+++ LeecodeChallenges/LeecodeChallenges/Solution_01480_RunningSumOf1dArray.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+public class Solution_01480_RunningSumOf1dArray
+{
+    public int[] RunningSum(int[] nums)
+    {
+        for (var i = 1; i < nums.Length; i++)
+        {
+            nums[i] = nums[i] + nums[i - 1];
+        }
+        return nums;
+    }
+}
