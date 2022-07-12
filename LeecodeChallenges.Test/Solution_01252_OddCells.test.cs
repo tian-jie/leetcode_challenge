1a654597472bd77648ae2ca8c86addf9af85b601
Tian Jie <jie.tian@outlook.com>
2022-07-12 21:43:35 +0800

    Solution_01252_OddCells

---
+++ LeecodeChallenges.Test/Solution_01252_OddCells.test.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Solution_01252_OddCells;
+using Xunit;
+
+namespace LeetcodeChallenges.Test
+{
+    public class Solution_01252_OddCellsTest
+    {
+        Solution obj = new Solution();
+
+        [Fact]
+        public void Case1()
+        {
+            var m = 2;
+            var n = 3;
+
+            var result = obj.OddCells(m, n, new int[][] { new int[] { 0, 1 }, new int[] { 1, 1 } });
+            Assert.Equal(6, result);
+        }
+
+        [Fact]
+        public void Case2()
+        {
+            var m = 2;
+            var n = 2;
+
+            var result = obj.OddCells(m, n, new int[][] { new int[] { 1, 1 }, new int[] { 0, 0 } });
+            Assert.Equal(0, result);
+        }
+
+
+
+
+
+    }
+}
---
+++ LeecodeChallenges/LeecodeChallenges/Solution_01252_OddCells.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System;
+using System.IO;
+
+namespace Solution_01252_OddCells
+{
+    public class Solution
+    {
+        public int OddCells(int m, int n, int[][] indices)
+        {
+            var arr = new int[n * m];
+
+            foreach (var indice in indices)
+            {
+                // 这一行上的所有内容
+                for (var i = indice[0] * n; i < indice[0] * n + n; i++)
+                {
+                    arr[i]++;
+                }
+
+                // 这一列的算法
+                for (var i = 0; i < m; i++)
+                {
+                    arr[indice[1] + n * i]++;
+                }
+            }
+            return arr.Count(a => a % 2 == 1);
+        }
+    }
+}
