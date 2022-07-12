5f27e9c0642c93f11deaa962787fd6e7d0169882
Tian Jie <jie.tian@outlook.com>
2022-07-12 20:33:20 +0800

    Solution_00676_MagicDictionary

---
+++ LeecodeChallenges.Test/Solution_00676_MagicDictionary.test.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Solution_00676_MagicDictionary;
+using Xunit;
+
+namespace LeetcodeChallenges.Test
+{
+    public class Solution_00676_MagicDictionary_Test
+    {
+        MagicDictionary obj = new MagicDictionary();
+
+        [Fact]
+        public void Case1()
+        {
+            obj.BuildDict(new string[] { "hello", "leetcode" });
+            var result = obj.Search("hello");
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Case2()
+        {
+            obj.BuildDict(new string[] { "hello", "leetcode" });
+            var result = obj.Search("hhllo");
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Case3()
+        {
+            obj.BuildDict(new string[] { "hello", "leetcode" });
+            var result = obj.Search("hell");
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Case4()
+        {
+            obj.BuildDict(new string[] { "hello", "leetcode" });
+            var result = obj.Search("leetcoded");
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Case5()
+        {
+            obj.BuildDict(new string[] { "hello", "leetcode" });
+            var result = obj.Search("leetcodd");
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Case6()
+        {
+            obj.BuildDict(new string[] { "hello", "hallo", "leetcode" });
+            var result = obj.Search("hello");
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Case7()
+        {
+            obj.BuildDict(new string[] { "hello", "hallo", "leetcode" });
+            var result = obj.Search("hell");
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Case8()
+        {
+            obj.BuildDict(new string[] { "a", "ab", "abc" });
+            var result = obj.Search("bbc");
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Case9()
+        {
+            obj.BuildDict(new string[] { "a", "b", "ab", "abc", "abcabacbababdbadbfaejfoiawfjaojfaojefaowjfoawjfoawj", "abcdefghijawefe", "aefawoifjowajfowafjeoawjfaow", "cba", "cas", "aaewfawi", "babcda", "bcd", "awefj" });
+            var result = obj.Search("bbc");
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Case10()
+        {
+            obj.BuildDict(new string[] { "a", "b", "ab", "abc", "abcabacbababdbadbfaejfoiawfjaojfaojefaowjfoawjfoawj", "abcdefghijawefe", "aefawoifjowajfowafjeoawjfaow", "cba", "cas", "aaewfawi", "babcda", "bcd", "awefj" });
+            var result = obj.Search("bb");
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Case11()
+        {
+            obj.BuildDict(new string[] { "a", "b", "ab", "abc", "abcd" });
+            var result = obj.Search("bb");
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Case12()
+        {
+            obj.BuildDict(new string[] { "a", "b", "ab", "abc" });
+            var result = obj.Search("bbc");
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Case13()
+        {
+            obj.BuildDict(new string[] { "a", "b", "ab" });
+            var result = obj.Search("bb");
+            Assert.True(result);
+        }
+
+
+
+
+    }
+}
---
+++ LeecodeChallenges/LeecodeChallenges/Solution_00676_MagicDictionary.cs
@@ -0,0 +1,90 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System;
+using System.IO;
+
+namespace Solution_00676_MagicDictionary
+{
+    public class MagicDictionary
+    {
+        Dictionary<char, object> _rootDictionary = new Dictionary<char, object>();
+        public MagicDictionary()
+        {
+
+        }
+
+        public void BuildDict(string[] dictionary)
+        {
+            foreach (var s in dictionary)
+            {
+                var currentDictionary = _rootDictionary;
+                foreach (var c in s)
+                {
+                    Dictionary<char, object> nextDictionary = null;
+                    if (!currentDictionary.ContainsKey(c))
+                    {
+                        nextDictionary = new Dictionary<char, object>();
+                        currentDictionary.Add(c, nextDictionary);
+                        currentDictionary = nextDictionary;
+                    }
+                    else
+                    {
+                        currentDictionary = (Dictionary<char, object>)currentDictionary[c];
+                    }
+                }
+                currentDictionary.Add('\0', null);
+            }
+        }
+
+        public bool Search(string searchWord)
+        {
+            return SearchPrivate(searchWord, 0, _rootDictionary, false);
+        }
+
+        private bool SearchPrivate(string searchWord, int level, Dictionary<char, object> currentDictionary, bool hasDifferent)
+        {
+            // 判断hasDifferent=true并且到终点了
+            if (currentDictionary == null)
+            {
+                return false;
+            }
+            if (level == searchWord.Length)
+            {
+                if (hasDifferent && currentDictionary != null && currentDictionary.ContainsKey('\0'))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+
+            // 没有字母被替换过的话，广度循环
+            foreach (var key in currentDictionary.Keys)
+            {
+                if (!hasDifferent)
+                {
+                    var nextDictionary = (Dictionary<char, object>)currentDictionary[key];
+                    var hasResult = SearchPrivate(searchWord, level + 1, nextDictionary, (!(key == searchWord[level])) || hasDifferent);
+
+                    if (hasResult)
+                    {
+                        return true;
+                    }
+                }
+                else
+                {
+                    // 有过被替换的字母，则不循环了，直接深度优先
+                    if (key == searchWord[level])
+                    {
+                        return SearchPrivate(searchWord, level + 1, (Dictionary<char, object>)currentDictionary[key], true);
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
